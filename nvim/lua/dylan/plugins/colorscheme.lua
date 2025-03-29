6c94ac334d6b6cfea0eef7eac280c77afcb2c698
Dylan Tran <dylanbt928@gmail.com>
2025-03-29 00:30:50 -0700

    Add nvim

---
+++ nvim/lua/dylan/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000, -- Ensure it loads first
+        config = function(_, opts)
+            vim.cmd("colorscheme rose-pine") -- Apply the colorscheme
+        end
+    },
+}
---
+++ nvim/lua/dylan/plugins/lsp/mason.lua
@@ -0,0 +1,53 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        -- import mason
+        local mason = require("mason")
+
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
+
+        -- import mason-tool-installer
+        local mason_tool_installer = require("mason-tool-installer")
+
+        -- Ensures Mason checks system PATH first
+        require("mason").setup({
+            PATH = "prepend", -- Ensures Mason checks system PATH first
+        })
+
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "clangd", -- C/C++ LSP
+            },
+        })
+
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "pylint",
+                "eslint_d",
+                "clang-format", -- C/C++ formatter
+                "cpplint", -- C/C++ linter
+            },
+        })
+    end,
+}
