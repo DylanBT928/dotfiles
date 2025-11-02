21f4071d973ef67fdcc77c82b173910fb9f3fc4e
Dylan Tran <dylanbt928@gmail.com>
2025-11-02 00:03:13 -0700

    update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,11 @@ set fish_greeting ""
 
 if status is-interactive
     oh-my-posh init fish --config ~/.config/fish/poshthemes/takuya.omp.json | source
+
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+
+    alias n='nvim'
+    alias ff='fastfetch'
+    alias ponysay='env PYTHONWARNINGS=ignore ponysay'
 end
