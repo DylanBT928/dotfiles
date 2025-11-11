3778d450aaec6a3cbb52a5489788afc7b7910248
Dylan Tran <dylanbt928@gmail.com>
2025-11-11 00:30:40 -0800

    update dotfiles

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,11 @@ return {
 	name = "sonokai",
 	priority = 1000,
 	config = function()
+        vim.g.sonokai_transparent_background = 2
 		vim.cmd("colorscheme sonokai")
+        vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
 		vim.api.nvim_set_hl(0, "LineNr", { fg = "#7c6756" })
 	end,
 }
